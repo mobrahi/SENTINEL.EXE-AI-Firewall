abe3d2ee370ea55a9f1b0c027ca64ee697ffd6f7
mobrahi <faairuz@gmail.com>
2026-01-31 04:49:14 +0000

    Syncing cloud deployment fixes to GitHub

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
